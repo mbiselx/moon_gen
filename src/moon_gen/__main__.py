2d4714c6414ae03a8a9114e91d9df25dc8c98f9c
Michael Biselx <mw.biselx@bluewin.ch>
2023-06-28 10:30:25 +0200

    added -n ("newest") option to cli

--- src/moon_gen/__main__.py
+++ src/moon_gen/__main__.py
@@ -9,10 +9,34 @@ from .surface_plotter import SurfacePlotter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('FILE', nargs='?', type=str, help='input file')
+parser.add_argument('-n', '--newest', action='store_true',
+                    help='use the most recently modified file in the module\'s `surfaces` folder')
 args = parser.parse_args()
+
+
+def get_most_recent_file() -> str:
+    surfaces_dir = os.path.join(os.path.dirname(__file__), 'surfaces')
+    print(surfaces_dir)
+    print(*os.listdir(surfaces_dir), sep='\n')
+
+    files = filter(
+        os.path.isfile,
+        map(
+            lambda p: os.path.join(surfaces_dir, p),
+            os.listdir(surfaces_dir)
+        )
+    )
+    files = sorted(files, key=os.path.getmtime)
+    return files[0]
+
 
 pg.mkQApp("GLSurfacePlot Example")
 with SurfacePlotter() as w:
     if args.FILE is not None:
         w.plotSurfaceFromFile(args.FILE)
+    elif args.newest:
+        file = get_most_recent_file()
+        print(f"using {file}")
+        w.plotSurfaceFromFile(file)
+
     sys.exit(pg.exec())
