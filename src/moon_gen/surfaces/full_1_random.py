77764660b015a3ebd0f3d288892f81c628b71bf4
Michael Biselx <mw.biselx@bluewin.ch>
2023-08-28 16:12:29 +0200

    small refactor

--- src/moon_gen/surfaces/full_1_random.py
+++ src/moon_gen/surfaces/full_1_random.py
@@ -18,9 +18,56 @@ __depends__ = [
 ]
 
 
+def parametric_surface(
+        x, y,
+        epochs=6,
+        ocatves=6,  # don't need many, bc weathering
+        psd=surface_psd_nominal,
+        distribution=crater_density_young,
+):
+
+    print("generating background")
+    z = perlin_multiscale_grid(
+        x,
+        y,
+        octaves=ocatves,
+        psd=psd,
+    )
+
+    distribution.d_min = 4*x.ptp()/len(x)
+    nb_craters = distribution.number(x, y)
+    print(f"generating {nb_craters} craters")
+
+    # create older craters first and weather them
+    for w in reversed(range(epochs)):
+        for _ in range(nb_craters//epochs):
+            d = distribution.diameter(np.random.random())
+            center = (x.ptp() * np.random.random() + x.min(),
+                      y.ptp() * np.random.random() + y.min())
+            z = make_crater(x, y, z, d/2, center)
+
+        if w > 0:
+            z = waste_gaussian(z, x.ptp()/len(x), w/epochs)
+
+    # apply micro-meteorite impacts
+    z += np.random.normal(scale=2e-2*x.ptp()/len(x), size=z.shape)
+
+    # create the last remaining craters unweathered
+    for _ in range(nb_craters % epochs):
+        d = distribution.diameter(np.random.random())
+        center = (x.ptp() * np.random.random() + x.min(),
+                  y.ptp() * np.random.random() + y.min())
+        z = make_crater(x, y, z, d/2, center)
+
+    print("done")
+
+    return z
+
+
 # def surface(n=1025) -> SurfaceType:
-def surface(n=513) -> SurfaceType:
-    # def surface(n=257) -> SurfaceType:
+# def surface(n=513) -> SurfaceType:
+# def surface(n=129) -> SurfaceType:
+def surface(n=130) -> SurfaceType:
     '''
     create a random lunar surface using:
      - mutliscale perlin grid with a lunar highland PSD
@@ -35,44 +82,8 @@ def surface(n=513) -> SurfaceType:
     x = np.linspace(-ax/2, ax/2, nx)
     y = np.linspace(-ay/2, ay/2, ny)
 
-    print("generating background")
-    z = perlin_multiscale_grid(
-        x+cx,
-        y+cy,
-        octaves=6,  # don't need many, bc weathering
-        psd=surface_psd_nominal,
-        # psd=surface_psd_smooth,
-    )
-
-    distribution = crater_density_young
-    distribution = crater_density_mature
-    # distribution = crater_density_old
-    distribution.d_min = 4*ax/n
-
-    nb_craters = distribution.number(x, y)
-    print(f"generating {nb_craters} craters")
-
-    # create older craters first and weather them
-    for w in reversed(range(epochs)):
-        for _ in range(nb_craters//epochs):
-            d = distribution.diameter(np.random.random())
-            center = (x.ptp() * np.random.random() + x.min(),
-                      y.ptp() * np.random.random() + y.min())
-            z = make_crater(x, y, z, d/2, center)
-
-        if w > 0:
-            z = waste_gaussian(z, ax/nx, w/epochs)
-
-    # apply micro-meteorite impacts
-    z += np.random.normal(scale=2e-2*ax/nx, size=z.shape)
-
-    # create the last remaining craters unweathered
-    for _ in range(nb_craters % epochs):
-        d = distribution.diameter(np.random.random())
-        center = (x.ptp() * np.random.random() + x.min(),
-                  y.ptp() * np.random.random() + y.min())
-        z = make_crater(x, y, z, d/2, center)
-
-    print("done")
+    z = parametric_surface(x+cx, y+cy, epochs,
+                           psd=surface_psd_nominal,
+                           distribution=crater_density_mature)
 
     return x, y, z
